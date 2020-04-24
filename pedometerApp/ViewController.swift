3de14842e35ed3a20287a0f07678d44be173ab0c
yewonblairseo@gmail.com <yewonblairseo@gmail.com>
2020-04-24 10:48:36 +0200

    Initial Commit

---
+++ pedometerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pedometerApp
+//
+//  Created by Yewon Seo on 2020/04/24.
+//  Copyright © 2020 Yewon Seo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
